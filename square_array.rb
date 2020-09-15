cf94119fc54889cbaff774f64334b721f0e5e1d9
Aiden Marques <aiden.marques94@gmail.com>
2020-09-15 18:58:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |number|
     new_array.push(number**=2)
 end
   new_array 
+=======
+  array.each do|x|
+  x**= 2
+end
+>>>>>>> d1727010c2dc0e1b4e1c1281b0ae3d435a1ac089
 end
